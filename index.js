7b39f29b548f6740924451271eb81c1a2437c202
tanjimTC <tanjimtc74@gmail.com>
2020-07-09 01:44:36 +0600

    ema-john-backend

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 4200;
+const port =  4200;
 
 app.use(cors());
 app.use(bodyParser.json());
